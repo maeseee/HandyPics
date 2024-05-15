2074be0ee350e3f690151dbb7cc384e2a6d44355
Marcel Mathis <maeseee@gmail.com>
2024-05-15 20:43:50 +0200

    validate destination path

--- src/main/java/org/ase/ConfigReader.java
+++ src/main/java/org/ase/ConfigReader.java
@@ -4,11 +4,14 @@ import com.google.common.net.InetAddresses;
 import lombok.Getter;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 @Getter
 public class ConfigReader {
+
+    private final static String DESTINATION_PATH = "C:\\Users\\maese\\Bilder\\FromHandy\\";
 
     private String ipAddress;
     private String folderName;
@@ -17,6 +20,8 @@ public class ConfigReader {
         while (ipAddress == null) {
             readFtpServerAddress();
         }
+
+        validateDestinationPath();
         while (folderName == null) {
             readFolderName();
         }
@@ -40,9 +45,17 @@ public class ConfigReader {
         final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter the folder name of the destination:");
         try {
-            folderName = reader.readLine();
+            String inputString = reader.readLine();
+            folderName = DESTINATION_PATH + inputString;
         } catch (IOException e) {
             throw new RuntimeException("Could not read folder name");
+        }
+    }
+
+    private void validateDestinationPath() {
+        boolean exists = new File(DESTINATION_PATH).exists();
+        if (!exists) {
+            throw new RuntimeException("Destination path does not exist");
         }
     }
 }
