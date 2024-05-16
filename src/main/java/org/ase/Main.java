af88f6ff1908e5cb7d04a16a5a4765b58f4d5159
Marcel Mathis <maeseee@gmail.com>
2024-05-16 20:36:12 +0200

    add copyFileFrom methode

--- src/main/java/org/ase/Main.java
+++ src/main/java/org/ase/Main.java
@@ -3,12 +3,29 @@ package org.ase;
 import org.ase.ftp.AndroidFtpClient;
 import org.ase.ftp.FtpAccessor;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+
+    private final static File LAST_BACKUP_TIME_PATH = new File("DCIM/"); // Do not save it on the root of the phone as it automatically gets deleted
+
+    public Main() {
         ConfigReader configReader = new ConfigReader();
         configReader.read();
 
         AndroidFtpClient ftpClient = new AndroidFtpClient(configReader.getIpAddress());
-        new FtpAccessor(ftpClient, configReader.getFolderPath());
+        FtpAccessor ftpAccessor = new FtpAccessor(ftpClient, configReader.getFolderPath());
+
+        try {
+            // TODO retry
+            ftpAccessor.copyFileFrom(LAST_BACKUP_TIME_PATH);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main();
     }
 }
--- src/main/java/org/ase/ftp/FtpAccessor.java
+++ src/main/java/org/ase/ftp/FtpAccessor.java
@@ -1,5 +1,6 @@
 package org.ase.ftp;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collection;
@@ -14,16 +15,22 @@ public class FtpAccessor {
         this.destinationDir = destinationDir;
     }
 
-    public void copyFiesFrom(Path sourceDir) throws IOException {
+    public void copyFilesFrom(Path sourcePath) throws IOException {
         ftpClient.open();
-        Collection<String> files = ftpClient.listFiles(sourceDir);
+        Collection<String> files = ftpClient.listFiles(sourcePath);
         files.forEach(filename -> {
             try {
-                ftpClient.downloadFile(sourceDir + "/" + filename, destinationDir + "/" + filename);
+                ftpClient.downloadFile(sourcePath + "/" + filename, destinationDir + "/" + filename);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         });
+        ftpClient.close();
+    }
+
+    public void copyFileFrom(File filePath) throws IOException {
+        ftpClient.open();
+        ftpClient.downloadFile(filePath.getPath(), destinationDir + "/" + filePath.getName());
         ftpClient.close();
     }
 }
