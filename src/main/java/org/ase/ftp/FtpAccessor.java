ae37f4de2dab86012ed33e79ad5762079f3e26e9
Marcel Mathis <maeseee@gmail.com>
2024-05-18 07:14:16 +0200

    Add file and folder filtering

--- src/main/java/org/ase/ftp/FtpAccessor.java
+++ src/main/java/org/ase/ftp/FtpAccessor.java
@@ -14,23 +14,42 @@ public class FtpAccessor {
     private final FtpClient ftpClient;
 
     public void copyFilesFrom(Path sourcePath, Path destinationPath, LocalDateTime lastBackupTime) throws IOException {
+        ftpClient.open();
         Collection<FileProperty> files = ftpClient.listFiles(sourcePath);
         List<Path> filteredList = files.stream()
                 .filter(fileProperty -> isModificationDateNewer(lastBackupTime, fileProperty.modificationDate()))
-                // TODO filter image data
                 .map(FileProperty::filePath)
+                .filter(this::isNotInFileIgnoreList)
+                .filter(this::isImageOrVideoFile)
                 .toList();
 
         for (Path filePath : filteredList) {
-            copyFileFrom(filePath, destinationPath.resolve(filePath.getFileName()));
+            download(filePath, destinationPath.resolve(filePath.getFileName()));
+        }
+        ftpClient.close();
+
+        callSubdirectories(sourcePath, destinationPath, lastBackupTime);
+    }
+
+    public void callSubdirectories(Path sourcePath, Path destinationPath, LocalDateTime lastBackupTime) throws IOException {
+        Collection<Path> directories = ftpClient.listDirectories(sourcePath);
+        List<Path> filteredList = directories.stream()
+                .filter(this::isNotInDirectoryIgnoreList)
+                .toList();
+        for (Path path : filteredList) {
+            copyFilesFrom(path, destinationPath, lastBackupTime);
         }
     }
 
-    public void copyFileFrom(Path sourcePath, Path destinationPath) throws IOException {
-        // TODO retry
-        ftpClient.open();
-        download(sourcePath, destinationPath);
-        ftpClient.close();
+    public void copyFileFrom(Path sourcePath, Path destinationPath) {
+        try {
+            ftpClient.open();
+            download(sourcePath, destinationPath);
+            ftpClient.close();
+        } catch (IOException e) {
+            // TODO retry
+            throw new RuntimeException(e);
+        }
     }
 
     private void download(Path sourcePath, Path destinationPath) throws IOException {
@@ -39,5 +58,27 @@ public class FtpAccessor {
 
     private boolean isModificationDateNewer(LocalDateTime lastBackupTime, LocalDateTime modificationDate) {
         return lastBackupTime.isBefore(modificationDate);
+    }
+
+    private boolean isNotInFileIgnoreList(Path path) {
+        List<String> ignoreList = List.of("trash");
+        String fileName = path.getFileName().toString().toLowerCase().trim();
+        return !ignoreList.contains(fileName);
+    }
+
+    private boolean isNotInDirectoryIgnoreList(Path path) {
+        List<String> ignoreList = List.of("sent", "private");
+        String folderName = path.getFileName().toString().toLowerCase().trim();
+        return !ignoreList.contains(folderName);
+    }
+
+    private boolean isImageOrVideoFile(Path path) {
+        List<String> fileEndings = List.of(
+                ".jpg", ".jpeg", ".png", ".giv", ".tiff", ".bmp", ".svg", // image files
+                ".mp4", ".avi", ".mkv", ".mov", ".avchd", ".h264", ".265" // video files
+        );
+
+        String folderName = path.getFileName().toString().toLowerCase().trim();
+        return !fileEndings.contains(folderName);
     }
 }
