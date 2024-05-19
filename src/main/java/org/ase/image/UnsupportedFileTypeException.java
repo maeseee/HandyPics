bea33cb4380ca819f4bf4d28a4143b4e4ca2aceb
Marcel Mathis <maeseee@gmail.com>
2024-05-19 20:57:01 +0200

    Remove unused code

--- src/main/java/org/ase/image/UnsupportedFileTypeException.java
+++ src/main/java/org/ase/image/UnsupportedFileTypeException.java
@@ -1,19 +1,7 @@
 package org.ase.image;
 
 public class UnsupportedFileTypeException extends Exception {
-    public UnsupportedFileTypeException() {
-        super();
-    }
-
     public UnsupportedFileTypeException(String message) {
         super(message);
-    }
-
-    public UnsupportedFileTypeException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public UnsupportedFileTypeException(Throwable cause) {
-        super(cause);
     }
 }
