25ba7151d3debc99d656460f5593d76faae51c2f
Marcel Mathis <maeseee@gmail.com>
2024-07-06 14:49:48 +0200

    Move favorit split into HandyPics

--- src/main/java/org/ase/HandyPics.java
+++ src/main/java/org/ase/HandyPics.java
@@ -1,14 +1,32 @@
 package org.ase;
 
+import com.google.common.collect.ImmutableList;
 import org.ase.config.Config;
 import org.ase.ftp.AndroidFtpClient;
 import org.ase.ftp.FtpAccessor;
 import org.ase.history.LastBackup;
+import org.ase.transfer.BackupFolder;
 import org.ase.transfer.TransferPictures;
 
+import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class HandyPics {
+
+    private final List<BackupFolder> FAVORIT_BACKUP_FOLDERS = ImmutableList.of(
+            new BackupFolder(Path.of("DCIM/MyAlbums/Best"), "Camera"), // Favourites on Oppo
+            new BackupFolder(Path.of("MIUI/Gallery/cloud/owner/best"), "Camera"), // Favourites old on Xiaomi
+            new BackupFolder(Path.of("Pictures/Gallery/owner/best"), "Camera"), // Favourites on Xiaomi
+            new BackupFolder(Path.of("MIUI/Gallery/cloud/owner"), "Camera") // Albums on Xiaomi
+    );
+    private final List<BackupFolder> BACKUP_FOLDERS = ImmutableList.of(
+            new BackupFolder(Path.of("DCIM"), "Camera"),
+            new BackupFolder(Path.of("Pictures"), "Signal"), // Signal on Oppo
+            new BackupFolder(Path.of("Bluetooth"), "Bluetooth"), // Bluetooth on Oppo
+            new BackupFolder(Path.of("MIUI/ShareMe"), "Bluetooth"), // Bluetooth on Xiaomi
+            new BackupFolder(Path.of("Android/media/com.whatsapp/WhatsApp/Media"), "Whatsapp")
+    );
 
     private final Config config;
     private final FtpAccessor ftpAccessor;
@@ -25,8 +43,13 @@ public class HandyPics {
     public void transferImagesFromHandy() {
         loadLastBackupTime();
 
-        TransferPictures transferPictures = new TransferPictures(ftpAccessor, lastBackupTime, config.destinationRootFolder());
-        transferPictures.copy();
+        TransferPictures transferFavoritPictures =
+                new TransferPictures(ftpAccessor, lastBackupTime, config.destinationRootFolder(), FAVORIT_BACKUP_FOLDERS);
+        transferFavoritPictures.copy(true);
+
+        TransferPictures transferPictures =
+                new TransferPictures(ftpAccessor, lastBackupTime, config.destinationRootFolder(), BACKUP_FOLDERS);
+        transferPictures.copy(false);
 
         // TODO save backup time
     }
--- src/main/java/org/ase/transfer/TransferPictures.java
+++ src/main/java/org/ase/transfer/TransferPictures.java
@@ -1,6 +1,5 @@
 package org.ase.transfer;
 
-import com.google.common.collect.ImmutableList;
 import lombok.AllArgsConstructor;
 import org.apache.commons.imaging.ImageReadException;
 import org.apache.commons.imaging.ImageWriteException;
@@ -19,40 +18,26 @@ import java.util.List;
 @AllArgsConstructor
 public class TransferPictures {
 
-    private final List<BackupFolder> favouriteBackupFolders = ImmutableList.of(
-            new BackupFolder(Path.of("DCIM/MyAlbums/Best"), "Camera"), // Favourites on Oppo
-            new BackupFolder(Path.of("MIUI/Gallery/cloud/owner/best"), "Camera"), // Favourites old on Xiaomi
-            new BackupFolder(Path.of("Pictures/Gallery/owner/best"), "Camera"), // Favourites on Xiaomi
-            new BackupFolder(Path.of("MIUI/Gallery/cloud/owner"), "Camera") // Albums on Xiaomi
-    );
-    private final List<BackupFolder> backupFolders = ImmutableList.of(
-            new BackupFolder(Path.of("DCIM"), "Camera"),
-            new BackupFolder(Path.of("Pictures"), "Signal"), // Signal on Oppo
-            new BackupFolder(Path.of("Bluetooth"), "Bluetooth"), // Bluetooth on Oppo
-            new BackupFolder(Path.of("MIUI/ShareMe"), "Bluetooth"), // Bluetooth on Xiaomi
-            new BackupFolder(Path.of("Android/media/com.whatsapp/WhatsApp/Media"), "Whatsapp")
-    );
-
     private final FtpAccessor accessor;
     private final LocalDateTime lastBackupTime;
     private final Path destinationRootFolder;
+    private final List<BackupFolder> backupFolders;
 
-    public void copy() {
-        favouriteBackupFolders.forEach(backupFolders -> copyFolder(backupFolders, true));
-        backupFolders.forEach(backupFolders -> copyFolder(backupFolders, false));
+    public void copy(boolean isFavorite) {
+        backupFolders.forEach(backupFolder -> copyFolder(backupFolder, isFavorite));
     }
 
-    private void copyFolder(BackupFolder backupFolders, boolean isFavorite) {
-        String favoriteSubFolderName = isFavorite ? backupFolders.destinationSubName() + "Favorite" : backupFolders.destinationSubName();
+    private void copyFolder(BackupFolder backupFolder, boolean isFavorite) {
+        String favoriteSubFolderName = isFavorite ? backupFolder.destinationSubName() + "Favorite" : backupFolder.destinationSubName();
         Path favoriteDestinationFolder = createDestinationFolder(favoriteSubFolderName);
         try {
-            accessor.copyFilesFrom(backupFolders.sourceFolder(), favoriteDestinationFolder, lastBackupTime);
+            accessor.copyFilesFrom(backupFolder.sourceFolder(), favoriteDestinationFolder, lastBackupTime);
         } catch (IOException e) {
             // TODO retry!
-            System.err.println("ERROR: " + backupFolders.sourceFolder() + " -> " + favoriteDestinationFolder + "\n" + e.getMessage());
+            System.err.println("ERROR: " + backupFolder.sourceFolder() + " -> " + favoriteDestinationFolder + "\n" + e.getMessage());
         }
         if (isFavorite) {
-            Path destinationFolder = createDestinationFolder(backupFolders.destinationSubName());
+            Path destinationFolder = createDestinationFolder(backupFolder.destinationSubName());
             setBestRating(favoriteDestinationFolder, destinationFolder);
         }
     }
